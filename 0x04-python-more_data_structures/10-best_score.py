7cc66af21a3b6122902a14e282bff44877443f6b
namubiru32 <84803600+namubiru32@users.noreply.github.com>
2021-09-15 19:54:10 +0000

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-def best_score(a_dictionary):
-    if not a_dictionary:
-        return None
-    return max(a_dictionary)
+#!/usr/bin/python3                                                             
+def best_score(my_dict):
+    return max(my_dict, key=my_dict.get) if my_dict else None
