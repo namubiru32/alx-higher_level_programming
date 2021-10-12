25cc188502475727b0b3fb323aa4e0b98bd4cfa2
namubiru32 <namubirubabra2@gmail.com>
2021-10-12 12:00:22 -0700

    class_to_json.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-"""Module 9-add_item.
-Adds all arguments to a Python list,
-and then save them to a file.
+"""
+Module 9-add_item
+Contains function that adds and saves to Python obj to JSON file; loads objects
+# run with ./9-add_item.py
+#
+# cat add_item.json ; echo ""
+# expect output: []
+#
+# ./9-add_item.py some random args
+# cat add_item.json ; echo ""
+# expect output: ["some", "random", "args"]
 """
 
-import sys
-import json
-import os.path
 
-save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
+from sys import argv
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-my_file = 'add_item.json'
+filename = "add_item.json"
 
-my_list = []
+try:
+    existing_content = load_from_json_file(filename)
+except FileNotFoundError:
+    existing_content = []
 
-if os.path.exists(my_file) and os.path.getsize(my_file) > 0:
-    my_list = load_from_json_file(my_file)
-
-if len(sys.argv) > 1:
-    for elem in sys.argv[1:]:
-        my_list.append(elem)
-
-save_to_json_file(my_list, my_file)
+save_to_json_file(existing_content + argv[1:], filename)
