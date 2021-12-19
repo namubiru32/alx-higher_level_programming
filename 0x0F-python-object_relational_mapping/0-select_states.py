e5d4ead7d748bde3c15cbb8789a27e7b66415188
namubiru32 <namubirubabra2@gmail.com>
2021-12-19 21:05:23 +0300

    a script that lists all states from the database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-""" list all files in hbtn_0e_0_usa order by states.id """
+"""lists all states from the database hbtn_0e_0_usa"""
+
 if __name__ == '__main__':
-    import MYSQLdb
+
+    import MySQLdb
     import sys
-    db = MYSQLdb.connect (host = "localhost", port = 3306, user = sys.argv[1], password = sys.argv[2], db = sys.argv[3])
-    mycursor = db.cursor()
-    mycursor.execute("SELECT * FROM states ORDER_BY states.id ASC;")
-    info = mycursor.fetchall()
-    for items in info:
-        print(items)
+
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
